b962159470c199f1b739c9248903fd6c908b897e
Manuel Máñez Ramón <mmanez@ainia.es>
2024-08-05 18:41:34 +0000

    Add filter to groups web

--- src/app/modules/core/web/group_web.py
+++ src/app/modules/core/web/group_web.py
@@ -1,9 +1,20 @@
-from fastapi import APIRouter, Request
+from typing import Annotated
+from fastapi import APIRouter, Depends, Request
 from src.app import main
+from src.app.modules.core.domain.group_service import GroupService
+from src.app.modules.core.domain.models import GroupFilter
+from src.app.modules.core.utils.paginator import PageParams, PageResponse
 
 router = APIRouter(prefix="/core/groups")
 
 
-@router.get("/form")
-async def form(request: Request):
-    return main.templates.TemplateResponse(request=request, name="core/group_form.html", context={})
+@router.get("/")
+async def form(
+    request: Request,
+    page_params: Annotated[PageParams, Depends()],
+    filter: Annotated[GroupFilter, Depends()],
+    service: Annotated[GroupService, Depends()],
+):
+    groups, total = service.read_all_paginated(page_params, filter)
+    page = PageResponse(page=page_params.page, size=page_params.size, total=total, content=groups)
+    return main.templates.TemplateResponse(request=request, name="core/group_list.html", context=page.model_dump())
