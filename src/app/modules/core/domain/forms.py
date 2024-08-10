a3488c1a2d8b60ac1d6183f4bbfb4a1d0e1dc9e0
Manuel Máñez Ramón <mmanez@ainia.es>
2024-08-10 14:46:13 +0000

    Add update group admin page

--- src/app/modules/core/domain/forms.py
+++ src/app/modules/core/domain/forms.py
@@ -1,19 +1,54 @@
 from fastapi import Request
 
 
-class GroupCreateForm:
+class Form:
     def __init__(self, request: Request):
         self.request: Request = request
         self.errors: dict[str, str] = {}
+
+    async def load(self) -> None:
+        pass
+
+    def is_valid(self) -> bool:
+        pass
+
+
+class GroupCreateForm(Form):
+    def __init__(self, request: Request):
+        super().__init__(request)
         self.code: str | None = None
         self.webname: str | None = None
 
-    async def load(self):
+    async def load(self) -> None:
         form = await self.request.form()
         self.code = form.get("code")
         self.webname = form.get("webname")
 
-    def is_valid(self):
+    def is_valid(self) -> bool:
+        valid = False
+        if not self.code or len(self.code) == 0:
+            self.errors["code"] = "code is required"
+        if not self.webname or len(self.webname) == 0:
+            self.errors["webname"] = "webname is required"
+        if not self.errors or len(self.errors) == 0:
+            valid = True
+        return valid
+
+
+class GroupUpdateForm(Form):
+    def __init__(self, request: Request):
+        super().__init__(request)
+        self.code: str | None = None
+        self.webname: str | None = None
+        self.active: bool
+
+    async def load(self) -> None:
+        form = await self.request.form()
+        self.code = form.get("code")
+        self.webname = form.get("webname")
+        self.active = form.get("active")
+
+    def is_valid(self) -> bool:
         valid = False
         if not self.code or len(self.code) == 0:
             self.errors["code"] = "code is required"
--- src/app/modules/core/web/group_web.py
+++ src/app/modules/core/web/group_web.py
@@ -2,10 +2,11 @@ from typing import Annotated
 from fastapi import APIRouter, Depends, Request
 from fastapi.responses import RedirectResponse
 from src.app import main
+from src.app.configuration.exceptions import EntityAlreadyExistsError
 from src.app.modules.core.domain.services.group_service import GroupService
-from src.app.modules.core.domain.models import GroupCreateCommand, GroupFilter
+from src.app.modules.core.domain.models import GroupCreateCommand, GroupFilter, GroupUpdateCommand
 from src.app.modules.core.utils.paginator import PageParams, PageResponse
-from src.app.modules.core.domain.forms import GroupCreateForm
+from src.app.modules.core.domain.forms import GroupCreateForm, GroupUpdateForm
 
 router = APIRouter(prefix="/core/groups")
 
@@ -37,14 +38,40 @@ async def group_create(request: Request):
 
 
 @router.post("/create")
-async def group_create_perform(
-    request: Request,
-    service: Annotated[GroupService, Depends()],
-):
+async def group_create_perform(request: Request, service: Annotated[GroupService, Depends()]):
     form = GroupCreateForm(request)
     await form.load()
+    context = form.__dict__
     if not form.is_valid():
+        return main.templates.TemplateResponse(request=request, name="core/group_create.html", context=context)
+    try:
+        form_data = {"code": form.code, "webname": form.webname}
+        service.create(GroupCreateCommand.model_validate(form_data))
+    except Exception as e:
+        context |= {"msg": e.msg, "type": "danger"}
         return main.templates.TemplateResponse(request=request, name="core/group_create.html", context=form.__dict__)
-    service.create(GroupCreateCommand.model_validate({"code": form.code, "webname": form.webname}))
+    redirect_ulr = request.url_for("group_list").include_query_params(msg="Successful operation")
+    return RedirectResponse(redirect_ulr, 303)
+
+
+@router.get("/update/{id}")
+async def group_update(request: Request, id: str, service: Annotated[GroupService, Depends()]):
+    group = service.read_by_id(id)
+    return main.templates.TemplateResponse(request=request, name="core/group_update.html", context=group.model_dump())
+
+
+@router.post("/update/{id}")
+async def group_update_perform(request: Request, id: str, service: Annotated[GroupService, Depends()]):
+    form = GroupUpdateForm(request)
+    await form.load()
+    context = form.__dict__
+    if not form.is_valid():
+        return main.templates.TemplateResponse(request=request, name="core/group_update.html", context=form.__dict__)
+    try:
+        form_data = {"code": form.code, "webname": form.webname, "active": form.active if form.active else False}
+        service.update(id, GroupUpdateCommand.model_validate(form_data))
+    except Exception as e:
+        context |= {"msg": e.msg, "type": "danger"}
+        return main.templates.TemplateResponse(request=request, name="core/group_update.html", context=form.__dict__)
     redirect_ulr = request.url_for("group_list").include_query_params(msg="Successful operation")
     return RedirectResponse(redirect_ulr, 303)
