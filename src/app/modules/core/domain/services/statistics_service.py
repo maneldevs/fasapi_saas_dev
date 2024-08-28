dc9d885191276ac3d76953f5b12d83b8550b1d1d
Manuel Máñez Ramón <mmanez@ainia.es>
2024-08-28 08:44:25 +0000

    Add modules to index page

--- src/app/modules/core/domain/services/statistics_service.py
+++ src/app/modules/core/domain/services/statistics_service.py
@@ -4,6 +4,7 @@ from fastapi import Depends
 
 from src.app.modules.core.domain.models import EntitiesCountResponse
 from src.app.modules.core.persistence.group_repo import GroupRepo
+from src.app.modules.core.persistence.module_repo import ModuleRepo
 from src.app.modules.core.persistence.role_repo import RoleRepo
 from src.app.modules.core.persistence.user_repo import UserRepo
 
@@ -15,16 +16,18 @@ class StatisticsService:
         group_repo: Annotated[GroupRepo, Depends()],
         role_repo: Annotated[RoleRepo, Depends()],
         user_repo: Annotated[UserRepo, Depends()],
+        module_repo: Annotated[ModuleRepo, Depends()],
     ) -> None:
         self.group_repo = group_repo
         self.role_repo = role_repo
         self.user_repo = user_repo
+        self.module_repo = module_repo
 
     def counts(self) -> EntitiesCountResponse:
         group_count = self.group_repo.count_all()
         role_count = self.role_repo.count_all()
         user_count = self.user_repo.count_all()
-        module_count = 0  # TODO mmr
+        module_count = self.module_repo.count_all()
         return EntitiesCountResponse(
             group_count=group_count, role_count=role_count, user_count=user_count, module_count=module_count
         )
